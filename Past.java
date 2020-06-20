076ce083f6b80afbb343460250718ea53d27c99d
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-06-19 23:59:01 -0400

    Create Past.java

---
+++ Past.java
@@ -0,0 +1,9 @@
+Convert given time into milliseconds 
+
+public class Clock
+{
+  public static int Past(int h, int m, int s) 
+  {
+     return ( h*60*60*1000 ) + ( m*60*1000 ) + ( s*1000 ); 
+  }
+}
