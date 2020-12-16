6e518ca0d641096cb26e4ef8fab524d0c3dd360b
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-12-16 12:04:38 -0600

    Update Extra Perfect Numbers.java

--- Extra Perfect Numbers.java
+++ Extra Perfect Numbers.java
@@ -6,11 +6,10 @@ Given a positive integer N , Return the extra perfect numbers in range from 1 to
         for (int i = 1; i<=number; i++) {
             String temp = Integer.toBinaryString(i);
             if (temp.charAt(0) == temp.charAt(temp.length()-1)) {
-                System.out.println("res " + temp + " " + i);
                 results.add(i);
             }
-
         }
+    
         int[] res = new int[results.size()];
         for (int i = 0; i<results.size(); i++) {
             res[i] = results.get(i);
