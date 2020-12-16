36b4c3808a726bc1c821ede205b59c8e556d2aeb
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-12-16 12:04:09 -0600

    Create Extra Perfect Numbers.java

---
+++ Extra Perfect Numbers.java
@@ -0,0 +1,19 @@
+
+Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
+
+    public static int[] extraPerfect(int number) {
+        List<Integer> results = new ArrayList<>();
+        for (int i = 1; i<=number; i++) {
+            String temp = Integer.toBinaryString(i);
+            if (temp.charAt(0) == temp.charAt(temp.length()-1)) {
+                System.out.println("res " + temp + " " + i);
+                results.add(i);
+            }
+
+        }
+        int[] res = new int[results.size()];
+        for (int i = 0; i<results.size(); i++) {
+            res[i] = results.get(i);
+        }
+        return res;
+    }
