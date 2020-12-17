8097cc1426bb14c92028736c7bd171878c078059
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-12-16 21:50:51 -0500

    Update Extra Perfect Numbers.java

--- Extra Perfect Numbers.java
+++ Extra Perfect Numbers.java
@@ -3,16 +3,12 @@ Given a positive integer N , Return the extra perfect numbers in range from 1 to
 
     public static int[] extraPerfect(int number) {
         List<Integer> results = new ArrayList<>();
+        
         for (int i = 1; i<=number; i++) {
             String temp = Integer.toBinaryString(i);
-            if (temp.charAt(0) == temp.charAt(temp.length()-1)) {
+            if (temp.charAt(0) == temp.charAt(temp.length() - 1)) {
                 results.add(i);
             }
         }
-    
-        int[] res = new int[results.size()];
-        for (int i = 0; i<results.size(); i++) {
-            res[i] = results.get(i);
-        }
-        return res;
+        return results.stream().mapToInt(Integer::intValue).toArray();
     }
