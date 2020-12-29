d878e12ce879b31b9401aef4ee18b7f867772a54
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-12-29 16:20:57 -0600

    Update MaxMinArrays.java

--- MaxMinArrays.java
+++ MaxMinArrays.java
@@ -1,36 +1,52 @@
-package com.company.katas;
+In this Kata, you will be given an array of unique elements, and your task is to rearrange the values so that the first max value is followed by the first 
+minimum, followed by second max value then second min value, etc.
 
+For example:
+
+solve([15,11,10,7,12]) = [15,7,12,10,11]
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MaxMinArrays {
     public static void main(String[] args) {
         int[] arr = { 15,11,10,7,12 };
         System.out.println(Arrays.toString(solve(arr)));
+
     }
 
     public static int[] solve (int[] arr){
         int[] result = new int[arr.length];
-        int[] arrCopy = new int[arr.length];
-        for (int i = 0; i < arr.length; i++ ) {
-            arrCopy[i] = arr[i];
+        List<Integer> myList = new ArrayList<Integer>(arr.length);
+        for (int i : arr) {
+            myList.add(i);
         }
 
         for (int k = 0; k < arr.length; k++ ) {
-            int max = Arrays.stream(arrCopy).max().orElse(0);
-            Arrays.stream(arrCopy).filter(val -> val != max);
-            System.out.println(max);
+            if (k%2 == 0) {
+                int max = Collections.max(myList);
+                result[k] = max;
+                for (int g = 0; g<myList.size(); g++ ) {
+                    if (myList.get(g) == max) {
+                        myList.remove(g);
+                    }
+                }
+            } else {
+                int min = Collections.min(myList);
+                result[k] = min;
+                for (int g = 0; g<myList.size(); g++ ) {
+                    if (myList.get(g) == min) {
+                        myList.remove(g);
+                    }
+                }
+            }
         }
-        System.out.println(Arrays.toString(arrCopy));
 
-        return arrCopy;
+        return result;
     }
 
-    public static int[] minMaxFinder(int[] arr) {
-        int[] res = new int[2];
-        int max = Arrays.stream(arr).max().orElse(0); 
-        int min = Arrays.stream(arr).min().orElse(0); 
-        res[0] = max; 
-        res[1] = min; 
-        return res; 
-    }
 }
+
