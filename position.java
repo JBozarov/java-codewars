8122c58824bbaa74cb0e743856f534b540548583
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-02-06 22:01:54 -0600

    Update position.java

--- position.java
+++ position.java
@@ -25,3 +25,17 @@ public class Position {
         position('z'); // 25
     }
 }
+
+
+
+Other solution: 
+public class Alphabet {
+    public static void main(String[] args) {
+        System.out.println(position("a"));
+    }
+
+    public static String position(String letter) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        return "Position of alphabet: " + (alphabet.indexOf(letter) + 1);
+    }
+}
